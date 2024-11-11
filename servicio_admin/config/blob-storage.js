15ff09dd545a8dfa0a184c92e5244e7063b75dd4
NicoG2023 <nguevarah@udistrital.edu.co>
2024-11-11 11:16:31 -0500

    Agregado CRUD para productos en vista productoController Agregada carpeta .devcontainer para trabajar dentro de los contenedores Eliminados volumenes en el docker-compose Read de Productos funcional

--- servicio_admin/config/blob-storage.js
+++ servicio_admin/config/blob-storage.js
@@ -1,11 +1,28 @@
-const { BlobServiceClient } = require("@azure/storage-blob");
+// config/blobStorage.js
+const { BlobServiceClient } = require('@azure/storage-blob');
 
 const AZURE_STORAGE_CONNECTION_STRING =
   process.env.AZURE_STORAGE_CONNECTION_STRING;
 
+if (!AZURE_STORAGE_CONNECTION_STRING) {
+  throw new Error(
+    'La cadena de conexión de Azure Storage no está configurada.',
+  );
+}
+
 const blobServiceClient = BlobServiceClient.fromConnectionString(
-  AZURE_STORAGE_CONNECTION_STRING
+  AZURE_STORAGE_CONNECTION_STRING,
 );
 const containerClient = blobServiceClient.getContainerClient(
-  "imagenes-artesanias"
+  'imagenes-artesanias',
 );
+
+if (!containerClient) {
+  throw new Error(
+    'Error al obtener el contenedor. Verifica el nombre del contenedor y la cadena de conexión.',
+  );
+}
+
+module.exports = {
+  containerClient,
+};
